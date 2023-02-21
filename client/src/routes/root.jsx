27c4f6237b64aed34f165e783155c3e309c2c0fc
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-02-20 21:48:03 -0600

    add contacts and edit done

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -1,6 +1,24 @@
-import { Outlet, Link } from 'react-router-dom';
+import { 
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+} from 'react-router-dom';
+import { getContacts, createContact } from "../contacts";
+
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 
 export default function Root() {
+  const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -24,19 +42,42 @@ export default function Root() {
                 aria-live="polite"
               ></div>
             </form>
-            <form method="post">
+            <Form method="post">
               <button type="submit">New</button>
-            </form>
+            </Form>
           </div>
           <nav>
+          {contacts.length ? (
             <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                   to={`contacts/${contact.id}`}
+                   className={({ isActive, isPending }) =>
+                   isActive
+                     ? "active"
+                     : isPending
+                     ? "pending"
+                     : ""
+                 }>
+                    
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
             </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           </nav>
         </div>
         <div id="detail">
