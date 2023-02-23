d6245ad3bf1e5bac9da51560017d30658759d3a6
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-02-22 20:56:07 -0600

    contact schema

---
+++ backend/models/Contact.js
@@ -0,0 +1,34 @@
+
+const ContactSchema = new mongoose.Schema({
+    contactFirstName: {
+      type: String,
+      required: true,
+    },
+     conctactLastName: {
+      type: String,
+      required: true,
+  },
+  contactInfo: {
+      type: String,
+      required: false,
+  },
+    notes: {
+      type: String,
+      required: true,
+  },
+  
+    completed: {
+      type: Boolean,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true
+    }
+  })
+  
+  module.exports = mongoose.model('Contact', ContactSchema)
+  
+  
+  
+  
